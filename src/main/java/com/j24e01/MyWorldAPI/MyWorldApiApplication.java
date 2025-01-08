b44ae335165de81e56c4e78a4c2e9599f3e16b5e
Vitalii <vitaliy.shekhovtsov@gmail.com>
2025-01-08 14:48:19 +0100

    Initial commit

---
+++ src/main/java/com/j24e01/MyWorldAPI/MyWorldApiApplication.java
@@ -0,0 +1,13 @@
+package com.j24e01.MyWorldAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWorldApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWorldApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/j24e01/MyWorldAPI/MyWorldApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j24e01.MyWorldAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWorldApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
